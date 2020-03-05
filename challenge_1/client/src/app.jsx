e39310d64fc40f4155da2a3a72ba3b19713c09bb
Noel Mendoza <nmendozavd@gmail.com>
2020-03-04 18:21:48 -0800

    getting data back from json

--- challenge_1/client/src/app.jsx
+++ challenge_1/client/src/app.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      events: []
+    }
 
+    this.getEvents = this.getEvents.bind(this);
   }
 
+  componentDidMount() {
+    this.getEvents();
+  }
+
+    // GET DATA
+  getEvents() {
+    axios.get(`/events`)
+      .then((response) => {
+        console.log(response.data);
+        this.setState({ data: response.data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
   render() {
     return (
