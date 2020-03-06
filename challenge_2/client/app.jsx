b43228ca290435ea8f5015a10985a1de2e64dfca
Noel Mendoza <nmendozavd@gmail.com>
2020-03-05 18:35:14 -0800

    added chart.js and inputted data for chart prices and dates from state

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -6,9 +6,7 @@ import Chart from 'chart.js';
 class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      data: []
-    };
+    this.state = {};
 
     this.getData = this.getData.bind(this);
   }
@@ -20,9 +18,12 @@ class App extends React.Component {
   getData() {
     axios.get('/prices')
       .then((response) => {
-        console.log(response.data);
+        // console.log(response.data.bpi);
+        var keys = Object.keys(response.data.bpi);
+        var values = Object.values(response.data.bpi);
         this.setState({
-          data: response.data
+          dates: keys,
+          prices: values
         })
       })
       .catch((error) => {
@@ -31,8 +32,33 @@ class App extends React.Component {
   }
 
   render() {
+    var ctx = 'myChart';
+
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: this.state.dates,
+        datasets: [
+          {
+            label: 'Bitcoin Value in USD',
+            data: this.state.prices,
+            fill: false,
+            pointRadius: 0,
+            pointHitRadius: '1',
+            pointStyle: 'circle',
+            borderColor: 'red',
+          }
+        ]
+      },
+    });
+
+
     return (
-      <div></div>
+      <div>
+        <h1>Cryptocurrency Charting Tool</h1>
+        <canvas id="myChart" ref={this.chartRef} width="1000" height="300"/>
+        <p>Powered by CoinDesk</p>
+      </div>
     )
   }
 }
