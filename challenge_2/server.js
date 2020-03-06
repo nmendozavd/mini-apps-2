8ae20bca2e4ca061778a739f02ede2fa14a61a45
Noel Mendoza <nmendozavd@gmail.com>
2020-03-05 16:32:24 -0800

    added api GET request and sending to client for 1 year worth of data

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,9 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const axios = require('axios');
 
 app.use(express.static('public'))
 
-// app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/prices', (req, res) => {
+  axios.get('https://api.coindesk.com/v1/bpi/historical/close.json?start=2013-09-01&end=2014-09-05')
+    .then((response) => {
+      // console.log(response.data);
+      var data = response.data;
+      res.status(200);
+      res.send(data);
+    })
+    .catch((error) => {
+      console.log(error)
+      res.status(400);
+    })
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.listen(port, () => console.log(`Ay dawg, Crypto listening on port ${port}!`))
