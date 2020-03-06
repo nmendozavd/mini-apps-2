f6a96787347bf47d4ea367b2f2693ba43f6534f3
Noel Mendoza <nmendozavd@gmail.com>
2020-03-06 12:38:35 -0800

    updated keypad and added scores to state

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -5,25 +5,40 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      totalScore: 0,
-      
+      maxPossible: 300,
+      currentScore: 0
+
     }
+    this.scoreHandler = this.scoreHandler.bind(this);
+  }
+
+  scoreHandler (event) {
+    console.log('testing score');
   }
 
   render() {
     return (
-      <div>
-        <h1>Bowling</h1>
-        <input type="button" value="7" />
-        <input type="button" value="8" />
-        <input type="button" value="9" /><br/>
-        <input type="button" value="4" />
-        <input type="button" value="5" />
-        <input type="button" value="6" /><br/>
-        <input type="button" value="1" />
-        <input type="button" value="2" />
-        <input type="button" value="3" /><br/>
+    <div>
+      <h1>Bowling</h1>
+      <div className="keypad">
+        <img src="bowling.jpg" alt="bowling" height="250" width="350"/><br/>
+        <h2>Max Score Possible: {this.state.maxPossible}</h2>
+        <h2>Current Score: {this.state.currentScore}</h2>
+        <br/>
+        <p>Click Number of Pins knocked Down</p>
+        <input type="button" value="7" onClick = {this.scoreHandler}/>
+        <input type="button" value="8" onClick={this.scoreHandler}/>
+        <input type="button" value="9" onClick={this.scoreHandler}/><br/>
+        <input type="button" value="4" onClick={this.scoreHandler}/>
+        <input type="button" value="5" onClick={this.scoreHandler}/>
+        <input type="button" value="6" onClick={this.scoreHandler}/><br/>
+        <input type="button" value="1" onClick={this.scoreHandler}/>
+        <input type="button" value="2" onClick={this.scoreHandler}/>
+        <input type="button" value="3" onClick={this.scoreHandler}/><br/>
+        <input type="button" value="X" onClick={this.scoreHandler}/>
+        <input type="button" value="0" onClick={this.scoreHandler}/>
       </div>
+    </div>
     )
   }
 
