a003d3c79c964e71904bfa2e9d4f45a962673bce
Noel Mendoza <nmendozavd@gmail.com>
2020-03-05 16:11:35 -0800

    added class component and axios GET request

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,4 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    };
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData() {
+    axios.get('/prices')
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('app'));
