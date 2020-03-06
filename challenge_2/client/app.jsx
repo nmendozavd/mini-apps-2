801ff31469fe589ac59569591fa0fdbb8d087ebd
Noel Mendoza <nmendozavd@gmail.com>
2020-03-05 16:46:34 -0800

    data from API now in state

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import Chart from 'chart.js';
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      data: []
     };
+
+    this.getData = this.getData.bind(this);
   }
 
   componentDidMount() {
@@ -17,7 +20,10 @@ class App extends React.Component {
   getData() {
     axios.get('/prices')
       .then((response) => {
-        console.log(response);
+        console.log(response.data);
+        this.setState({
+          data: response.data
+        })
       })
       .catch((error) => {
         console.log(error);
