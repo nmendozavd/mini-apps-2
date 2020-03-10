f505fbddae65c561f6671fa6537dc8cc15db2866
Noel Mendoza <nmendozavd@gmail.com>
2020-03-10 00:21:47 -0700

    added scoreHandler for simple game of 10 frames and newGame button

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -5,34 +5,33 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      score: 0,
       totalScore: 0,
-      roll: 1,
       frame: 1,
-      game: [[], [], [], [], [], [], [], [], [], []],
-      lastFrame: []
-
     }
     this.scoreHandler = this.scoreHandler.bind(this);
+    this.newGame = this.newGame.bind(this);
   }
 
-  scoreHandler (event) {
-    // console.log('testing score');
-    // frame, only 2 frames except for the 10th frame
-    var pinfall = event.target.value;
-    var frame = this.state.frame;
-    var roll = this.state.roll;
-    // check to see if during second roll they exceed the max 10 score per frame
-    if (roll === 2 && pinfall > 10) {
-      alert("You can't knockdown more than 10 pins in a frame!")
-      return this.setState({
-        roll: 2,
-        frame: this.state.frame
+  scoreHandler (score) {
+    // console.log(score);
+    if (this.state.frame <= 10) {
+      let newFrame = this.state.frame + 1;
+      let newTotalScore = this.state.totalScore + score;
+
+      this.setState({
+        frame: newFrame,
+        totalScore: newTotalScore
       })
-    }
+      } else {
+        alert('Game is over! You have played 10 frames.');
+      }
+  }
 
-
-
+  newGame() {
+    this.setState({
+      frame: 0,
+      totalScore: 1
+    })
   }
 
   render() {
@@ -40,23 +39,24 @@ class App extends React.Component {
     <div className="container">
       <h1>Bowling</h1>
       <img src="bowling.jpg" alt="bowling" height="250" width="350"/><br/>
-      <h3>Frame: {this.state.frame}</h3>
+      <h3>Frame # {this.state.frame}</h3>
       <h3>Current Score: {this.state.totalScore}</h3>
       <p>Click Number of Pins knocked Down</p>
       <div className='keypad'>
-        <input type="button" id = "button" value="7" onClick = {this.scoreHandler}/>
-        <input type="button" id="button" value="8" onClick={this.scoreHandler}/>
-        <input type="button" id="button" value="9" onClick={this.scoreHandler}/><br/>
-        <input type="button" id="button" value="4" onClick={this.scoreHandler}/>
-        <input type="button" id="button" value="5" onClick={this.scoreHandler}/>
-        <input type="button" id="button" value="6" onClick={this.scoreHandler}/><br/>
-        <input type="button" id="button" value="1" onClick={this.scoreHandler}/>
-        <input type="button" id="button" value="2" onClick={this.scoreHandler}/>
-        <input type="button" id="button" value="3" onClick={this.scoreHandler}/><br/>
-        <input type="button" id="button" value="X" onClick={this.scoreHandler}/>
-        <input type="button" id="button" value="0" onClick={this.scoreHandler}/>
+        <input type="button" id="button" value="7" onClick={() => this.scoreHandler(7)}/>
+        <input type="button" id="button" value="8" onClick={() => this.scoreHandler(8)}/>
+        <input type="button" id="button" value="9" onClick={() => this.scoreHandler(9)}/><br/>
+        <input type="button" id="button" value="4" onClick={() => this.scoreHandler(4)}/>
+        <input type="button" id="button" value="5" onClick={() => this.scoreHandler(5)}/>
+        <input type="button" id="button" value="6" onClick={() => this.scoreHandler(6)}/><br/>
+        <input type="button" id="button" value="1" onClick={() => this.scoreHandler(1)}/>
+        <input type="button" id="button" value="2" onClick={() => this.scoreHandler(2)}/>
+        <input type="button" id="button" value="3" onClick={() => this.scoreHandler(3)}/><br/>
+        <input type="button" id="button" value="X" onClick={() => this.scoreHandler(10)}/>
+        <input type="button" id="button" value="0" onClick={() => this.scoreHandler(0)}/>
       </div>
-
+      <br></br>
+        <button id="button1" onClick={() => this.newGame()}>New Game</button>
     </div>
     )
   }
